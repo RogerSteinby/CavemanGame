87a204f8049748cb941476a8ecfd42e49f2a201f
RogerSteinby <maccroc@gmail.com>
2024-09-07 11:49:22 -0400

    forgot this one

--- build.c
+++ build.c
@@ -37,7 +37,8 @@ typedef struct Context_Extra {
 //
 
 // This is a minimal starting point for new projects. Copy & rename to get started
-#include "oogabooga/examples/minimal_game_loop.c"
+//#include "oogabooga/examples/minimal_game_loop.c" // This is the example project
+#include "CavemanGame.c"
 
 // #include "oogabooga/examples/text_rendering.c"
 // #include "oogabooga/examples/custom_logger.c"
